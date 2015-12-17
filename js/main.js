86fd2332581d6c99aec485f8d24faccd6078f9c9
Klajdi Bejko <kbejko@gmail.com>
2015-12-17 01:13:03 -0500

    Add some js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var button = document.querySelector("button")
+var diu = document.getElementById("diu")
+var content = document.querySelector("span")
+button.addEventListener("click", function(){
+  if (diu.className === "") {
+    diu.className = "diu"
+    content.innerHTML = "if you've had enough of this nonsense"
+  } else {
+    diu.className = diu.className.replace("diu", "")
+    content.innerHTML = "to see a picture of my grandpa peeing on love"
+  }
+})
