baf8720cc6eed49745734c9b1fdadd31d697b77a
Klajdi Bejko <kbejko@gmail.com>
2015-12-23 00:20:28 -0500

    Navigation experiment

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@
 
 (function() {
   var body = document.body;
-  var burgerMenu = document.getElementsByClassName('b-menu')[0];
-  var burgerContain = document.getElementsByClassName('b-container')[0];
-  var burgerNav = document.getElementsByClassName('b-nav')[0];
-
-  burgerMenu.addEventListener('click', function toggleClasses() {
-    [body, burgerContain, burgerNav].forEach(function (el) {
-      el.classList.toggle('open');
-    });
-  }, false);
+  var nav = document.getElementsByClassName('nav');
+  // var burgerContain = document.getElementsByClassName('b-container')[0];
+  // var burgerNav = document.getElementsByClassName('b-nav')[0];
 })();
