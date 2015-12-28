4e496b95169754ed1c36b4b479d5d3a0f1709ba5
Klajdi Bejko <kbejko@gmail.com>
2015-12-28 14:56:57 -0500

    Adds finger pointing navigation. Needs interaction

--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
-var firstLi = document.getElementsByTagName("li")[0]
-var firstA = document.getElementsByTagName("a")[0]
-firstLi.onclick = function(){
-  // console.log("Hello")
-  firstA.classList.remove("active")
-}
+
